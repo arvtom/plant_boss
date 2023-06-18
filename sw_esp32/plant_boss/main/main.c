1880c4aabce467996b1fbccc68507a7faac2ab68
arvtom <tomkusarvydas@gmail.com>
2023-06-18 10:26:49 +0300

    Read temperature with ADC. Need to change vcc pin to io32 on pcba

--- sw_esp32/plant_boss/main/main.c
+++ sw_esp32/plant_boss/main/main.c
@@ -30,9 +30,26 @@ void app_main(void)
     // esp_err_t adc_oneshot_config_channel(adc_oneshot_unit_handle_t handle, adc_channel_t channel, const adc_oneshot_chan_cfg_t *config);
     // esp_err_t adc_oneshot_read(adc_oneshot_unit_handle_t handle, adc_channel_t chan, int *out_raw);
 
-    // esp_err_t gpio_config(const gpio_config_t *pGPIOConfig);
-    // esp_err_t gpio_set_level(gpio_num_t gpio_num, uint32_t level);
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
 
+    err_esp = adc_oneshot_new_unit(&init_config1, &adc1_handle);
+    printf("err_esp %d\n", (int)err_esp);
+
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_11,
+    };
+
+    err_esp = adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_3, &config);
+    printf("err_esp %d\n", (int)err_esp);
+
+    int adc_raw = 0;
+
+    /* external led pin */
     gpio_config_t io_conf_27 = {};
     io_conf_27.intr_type = GPIO_INTR_DISABLE;
     io_conf_27.mode = GPIO_MODE_OUTPUT;
@@ -42,6 +59,7 @@ void app_main(void)
     err_esp = gpio_config(&io_conf_27);
     printf("err_esp %d\n", (int)err_esp);
 
+    /* light sensor power supply pin */
     gpio_config_t io_conf_4 = {};
     io_conf_4.intr_type = GPIO_INTR_DISABLE;
     io_conf_4.mode = GPIO_MODE_OUTPUT;
@@ -51,6 +69,7 @@ void app_main(void)
     err_esp = gpio_config(&io_conf_4);
     printf("err_esp %d\n", (int)err_esp);
 
+    /* light sensor reset pin */
     gpio_config_t io_conf_17 = {};
     io_conf_17.intr_type = GPIO_INTR_DISABLE;
     io_conf_17.mode = GPIO_MODE_OUTPUT;
@@ -59,6 +78,22 @@ void app_main(void)
     io_conf_17.pull_up_en = 0;
     err_esp = gpio_config(&io_conf_17);
     printf("err_esp %d\n", (int)err_esp);
+
+#warning rewire_temperature_vcc_to_io32
+    /* temperature sensor power supply pin */
+    gpio_config_t io_conf_32 = {};
+    io_conf_32.intr_type = GPIO_INTR_DISABLE;
+    io_conf_32.mode = GPIO_MODE_OUTPUT;
+    io_conf_32.pin_bit_mask = (1ULL<<32u);
+    io_conf_32.pull_down_en = 0;
+    io_conf_32.pull_up_en = 0;
+    err_esp = gpio_config(&io_conf_32);
+    printf("err_esp %d\n", (int)err_esp);
+
+
+
+    err_esp = gpio_set_level(32u, 1);
+    printf("err_esp %d\n", (int)err_esp); 
 
     err_esp = gpio_set_level(4u, 1);
     printf("err_esp %d\n", (int)err_esp);
@@ -95,7 +130,11 @@ void app_main(void)
 
         err_esp = i2c_master_read_from_device(i2c_master_port, 0b0100011, (uint8_t *)buf_i2c_rx, 2, 100);
         printf("err_esp %d\n", (int)err_esp);
-        printf("buf 0x%x%x\n", buf_i2c_rx[0], buf_i2c_rx[1]);
+        printf("buf_i2c_rx 0x%x%x\n", buf_i2c_rx[0], buf_i2c_rx[1]);
+
+        err_esp = adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, &adc_raw);
+        printf("err_esp %d\n", (int)err_esp);
+        printf("adc_raw %d\n", adc_raw);
 
         vTaskDelay(100);
         // vTaskDelay(1000 / portTICK_PERIOD_MS);
