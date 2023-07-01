3a1fe39751b76ead57e4ba06d2a5669bbbd7e2a7
arvtom <tomkusarvydas@gmail.com>
2023-07-01 15:13:25 +0300

    created threads

--- sw_esp32/plant_boss/main/main.c
+++ sw_esp32/plant_boss/main/main.c
@@ -9,28 +9,79 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#define STACK_THREAD_APP            4096u
 #define STACK_THREAD_INPUT          4096u
-#define PRIORITY_THREAD_INPUT       10u
+#define STACK_THREAD_OUTPUT         4096u
+#define STACK_THREAD_NETWORK        4096u
+#define STACK_THREAD_MEMORY         4096u
 
+#define PRIORITY_THREAD_APP         10u
+#define PRIORITY_THREAD_INPUT       10u
+#define PRIORITY_THREAD_OUTPUT      10u
+#define PRIORITY_THREAD_NETWORK     10u
+#define PRIORITY_THREAD_MEMORY      10u
+
+TaskHandle_t addr_thread_app = NULL;
 TaskHandle_t addr_thread_input = NULL;
+TaskHandle_t addr_thread_output = NULL;
+TaskHandle_t addr_thread_network = NULL;
+TaskHandle_t addr_thread_memory = NULL;
+
+void thread_app(void *arg)
+{
+    while(1)
+    {
+        printf("app\n");
+        vTaskDelay(100);
+    }
+}
 
 void thread_input(void *arg)
 {
-    while(1){
-        printf("thread input\n");
+    while(1)
+    {
+        printf("input\n");
+        vTaskDelay(100);
+    }
+}
+
+void thread_output(void *arg)
+{
+    while(1)
+    {
+        printf("output\n");
+        vTaskDelay(100);
+    }
+}
+
+void thread_network(void *arg)
+{
+    while(1)
+    {
+        printf("network\n");
+        vTaskDelay(100);
+    }
+}
+
+void thread_memory(void *arg)
+{
+    while(1)
+    {
+        printf("memory\n");
         vTaskDelay(100);
     }
 }
 
 void app_main(void)
 {
+    xTaskCreate(thread_app, "thread_app", 
+        STACK_THREAD_APP, NULL, PRIORITY_THREAD_APP, &addr_thread_app);
     xTaskCreate(thread_input, "thread_input", 
         STACK_THREAD_INPUT, NULL, PRIORITY_THREAD_INPUT, &addr_thread_input);
-
-    // while(1)
-    // {
-        
-
-
-    // }
+    xTaskCreate(thread_output, "thread_output", 
+        STACK_THREAD_OUTPUT, NULL, PRIORITY_THREAD_OUTPUT, &addr_thread_output);
+    xTaskCreate(thread_network, "thread_network", 
+        STACK_THREAD_NETWORK, NULL, PRIORITY_THREAD_NETWORK, &addr_thread_network);
+    xTaskCreate(thread_memory, "thread_memory", 
+        STACK_THREAD_MEMORY, NULL, PRIORITY_THREAD_MEMORY, &addr_thread_memory);
 }
