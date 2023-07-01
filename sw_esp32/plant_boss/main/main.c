b9b01414e62845e74d506cd75ef34307a0c3b1fd
arvtom <tomkusarvydas@gmail.com>
2023-07-01 15:05:41 +0300

    create one thread

--- sw_esp32/plant_boss/main/main.c
+++ sw_esp32/plant_boss/main/main.c
@@ -9,14 +9,28 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#define STACK_THREAD_INPUT          4096u
+#define PRIORITY_THREAD_INPUT       10u
+
+TaskHandle_t addr_thread_input = NULL;
+
+void thread_input(void *arg)
+{
+    while(1){
+        printf("thread input\n");
+        vTaskDelay(100);
+    }
+}
+
 void app_main(void)
 {
+    xTaskCreate(thread_input, "thread_input", 
+        STACK_THREAD_INPUT, NULL, PRIORITY_THREAD_INPUT, &addr_thread_input);
 
-
-    while(1)
-    {
+    // while(1)
+    // {
         
 
 
-    }
+    // }
 }
