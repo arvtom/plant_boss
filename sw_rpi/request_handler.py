1d4eba4c03ffc4533a6ae79b14cff51fe34f0817
arvtom <tomkusarvydas@gmail.com>
2023-07-11 20:52:06 +0300

    comments

--- sw_rpi/request_handler.py
+++ sw_rpi/request_handler.py
@@ -1,25 +1,30 @@
 import sqlite3
 import cgi
 
-PATH_2_DB = '/home/pi/github/plant_boss/sw_rpi/plant_boss.db'
+PATH_DATABASE = '/home/pi/github/plant_boss/sw_rpi/plant_boss.database'
 TABLE_NAME = 'plant_boss_test_2'
 ROWS = 100
 
-## Dispatches HTTP requests to the appropriate handler.
+# Dispatches HTTP requests to the appropriate handler.
 def application(env, start_line):
-    if env['REQUEST_METHOD'] == 'POST':   ## add new DB record
+    if env['REQUEST_METHOD'] == 'POST':
+        # take new database entry
         return handle_post(env, start_line)
-    elif env['REQUEST_METHOD'] == 'GET':  ## create HTML-fragment report 
+
+    elif env['REQUEST_METHOD'] == 'GET':
+        # return database contents in html
         return handle_get(start_line)
-    else:                                 ## no other option for now
+
+    else:
+        # for other type html requests, return error
         start_line('405 METHOD NOT ALLOWED', [('Content-Type', 'text/plain')])
         response_body = 'Only POST and GET verbs supported.'
         return [response_body.encode()]                            
 
 def handle_post(env, start_line):    
-    form = get_field_storage(env)  ## body of an HTTP POST request
+    form = get_field_storage(env)  # body of an HTTP POST request
     
-    ## Extract fields from POST form.
+    # Extract fields from POST form.
     timestamp = form.getvalue('timestamp')
     device = form.getvalue('device')
     humidity = form.getvalue('humidity')
@@ -28,26 +33,28 @@ def handle_post(env, start_line):
     bat_voltage = form.getvalue('bat_voltage')
     rssi_wifi = form.getvalue('rssi_wifi')
 
-    ## Missing info?
+    # Check if there is missing data
     if (timestamp is not None and
-        device is not None and
-        humidity is not None and
-        light is not None and
-        temperature is not None and
-        bat_voltage is not None and
-        rssi_wifi is not None):
+            device is not None and
+            humidity is not None and
+            light is not None and
+            temperature is not None and
+            bat_voltage is not None and
+            rssi_wifi is not None):
         add_record(timestamp, device, humidity, light, temperature, bat_voltage, rssi_wifi)
         response_body = "POST request handled.\n"
         start_line('201 OK', [('Content-Type', 'text/plain')])
+
     else:
+        # Return error
         response_body = "Missing info in POST request.\n"
         start_line('400 Bad Request', [('Content-Type', 'text/plain')])
  
     return [response_body.encode()]
 
 def handle_get(start_line):
-    conn = sqlite3.connect(PATH_2_DB)        ## connect to DB
-    cursor = conn.cursor()                   ## get a cursor
+    conn = sqlite3.connect(PATH_DATABASE)        # connect to database
+    cursor = conn.cursor()                   # get a cursor
     cursor.execute("select * from " + TABLE_NAME)
     rows = cursor.fetchall()
 
@@ -77,17 +84,16 @@ def handle_get(start_line):
                 """
 
     for i in range(ROWS-1, 0, -1):
-        print(i)
         row = rows[i]
         response_body += "<tr>"
-        response_body += "<td>" + str(row[0]) + "</td>"     ## id
-        response_body += "<td>" + str(row[1]) + "</td>"     ## timestamp
-        response_body += "<td>" + str(row[2]) + "</td>"     ## device
-        response_body += "<td>" + str(row[3]) + "</td>"     ## humidity
+        response_body += "<td>" + str(row[0]) + "</td>"     # id
+        response_body += "<td>" + str(row[1]) + "</td>"     # timestamp
+        response_body += "<td>" + str(row[2]) + "</td>"     # device
+        response_body += "<td>" + str(row[3]) + "</td>"     # humidity
         response_body += "<td>" + str(row[4]) + "</td>"     #light
         response_body += "<td>" + str(row[5]) + "</td>"     #temperature
         response_body += "<td>" + str(row[6]) + "</td>"     #bat_voltage
-        response_body += "<td>" + str(row[7]) + "</td>"     ## rssi_wifi
+        response_body += "<td>" + str(row[7]) + "</td>"     # rssi_wifi
         response_body += "</tr>"
 
     response_body += """
@@ -96,16 +102,15 @@ def handle_get(start_line):
         </html>
         """
 
-    conn.commit()  ## commit
-    conn.close()   ## cleanup
+    conn.commit()
+    conn.close()
     
     start_line('200 OK', [('Content-Type', 'text/html')])
     return [response_body.encode()]
 
-## Add a record from a device to the DB.
 def add_record(timestamp, device, humidity, light, temperature, bat_voltage, rssi_wifi):
-    conn = sqlite3.connect(PATH_2_DB)      ## connect to DB
-    cursor = conn.cursor()                 ## get a cursor
+    conn = sqlite3.connect(PATH_DATABASE)      # connect to database
+    cursor = conn.cursor()                 # get a cursor
 
     print("Count of Rows")
     cursor.execute("select * from " + TABLE_NAME)
@@ -113,13 +118,13 @@ def add_record(timestamp, device, humidity, light, temperature, bat_voltage, rss
     print(number_rows)
 
     sql = "INSERT INTO " + TABLE_NAME + "(timestamp,device,humidity,light,temperature,bat_voltage,rssi_wifi) values (?,?,?,?,?,?,?)"
-    cursor.execute(sql, (timestamp, device, humidity, light, temperature, bat_voltage, rssi_wifi)) ## execute INSERT 
+    cursor.execute(sql, (timestamp, device, humidity, light, temperature, bat_voltage, rssi_wifi)) # execute INSERT 
 
     sql = "DELETE FROM " + TABLE_NAME +  " WHERE ROWID IN (SELECT ROWID FROM " + TABLE_NAME + " ORDER BY ROWID DESC LIMIT -1 OFFSET "+ str(ROWS) +")"
     cursor.execute(sql)
 
-    conn.commit()  ## commit
-    conn.close()   ## cleanup
+    conn.commit()
+    conn.close()
 
 def get_field_storage(env):
     input = env['wsgi.input']
@@ -127,7 +132,6 @@ def get_field_storage(env):
     if (form is not None and form[0] is input):
         return form[2]
 
-    fs = cgi.FieldStorage(fp = input,
-                          environ = env,
-                          keep_blank_values = 1)
+    fs = cgi.FieldStorage(fp = input, environ = env, keep_blank_values = 1)
+    
     return fs
