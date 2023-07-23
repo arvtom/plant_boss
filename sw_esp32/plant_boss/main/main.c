b9585063dcb4883b395157323a68f8cc82ebab6b
arvtom <tomkusarvydas@gmail.com>
2023-07-23 14:35:11 +0300

    create main.h

--- sw_esp32/plant_boss/main/main.c
+++ sw_esp32/plant_boss/main/main.c
@@ -9,23 +9,12 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#include "main.h"
 #include "thread_app.h"
 #include "thread_input.h"
 #include "thread_output.h"
 #include "thread_network.h"
 #include "thread_memory.h"
-
-#define STACK_THREAD_APP            4096u
-#define STACK_THREAD_INPUT          4096u
-#define STACK_THREAD_OUTPUT         4096u
-#define STACK_THREAD_NETWORK        4096u
-#define STACK_THREAD_MEMORY         4096u
-
-#define PRIORITY_THREAD_APP         10u
-#define PRIORITY_THREAD_INPUT       10u
-#define PRIORITY_THREAD_OUTPUT      10u
-#define PRIORITY_THREAD_NETWORK     10u
-#define PRIORITY_THREAD_MEMORY      10u
 
 TaskHandle_t addr_thread_app = NULL;
 TaskHandle_t addr_thread_input = NULL;
---
+++ sw_esp32/plant_boss/main/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define STACK_THREAD_APP            4096u
+#define STACK_THREAD_INPUT          4096u
+#define STACK_THREAD_OUTPUT         4096u
+#define STACK_THREAD_NETWORK        4096u
+#define STACK_THREAD_MEMORY         4096u
+
+#define PRIORITY_THREAD_APP         10u
+#define PRIORITY_THREAD_INPUT       10u
+#define PRIORITY_THREAD_OUTPUT      10u
+#define PRIORITY_THREAD_NETWORK     10u
+#define PRIORITY_THREAD_MEMORY      10u
+
+#endif
