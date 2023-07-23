1bcdc88c55e5091c5eaa978f11487f19bf5874c3
arvtom <tomkusarvydas@gmail.com>
2023-07-23 10:32:02 +0300

    definitions for notifications

--- sw_esp32/plant_boss/inc/common_thread_objects.h
+++ sw_esp32/plant_boss/inc/common_thread_objects.h
@@ -13,34 +13,227 @@
 
 /* ---------------------------- Typedefs ---------------------------- */
 
-#define THREAD_MEMORY_TO_APP_RESULT_INIT        0x00000001
-#define THREAD_APP_TO_NETWORK_REQUEST_INIT      0x00000002
-#define THREAD_NETWORK_TO_APP_RESULT_INIT       0x00000004
+typedef enum
+{
+    NOTIFICATION_TO_INPUT_REQ_INIT                      = 0x00000001,     /* bit 0 */
+    NOTIFICATION_TO_INPUT_REQ_HANDLE_ALL                = 0x00000002,     /* bit 1 */
+    NOTIFICATION_TO_INPUT_REQ_HANDLE_BATTERY            = 0x00000004,     /* bit 2 */
+    NOTIFICATION_TO_INPUT_RES_HANDLE_PWR_HUMIDITY       = 0x00000008,     /* bit 3 */
+    NOTIFICATION_TO_INPUT_RES_HANDLE_PWR_TEMPERATURE    = 0x00000010,     /* bit 4 */
+    NOTIFICATION_TO_INPUT_RES_HANDLE_PWR_LIGHT          = 0x00000020,     /* bit 5 */
+    // NOTIFICATION_TO_ = 0x00000040,     /* bit 6 */
+    // NOTIFICATION_TO_ = 0x00000080,     /* bit 7 */
+    // NOTIFICATION_TO_ = 0x00000100,     /* bit 8 */
+    // NOTIFICATION_TO_ = 0x00000200,     /* bit 9 */
+    // NOTIFICATION_TO_ = 0x00000400,     /* bit 10 */
+    // NOTIFICATION_TO_ = 0x00000800,     /* bit 11 */
+    // NOTIFICATION_TO_ = 0x00001000,     /* bit 12 */
+    // NOTIFICATION_TO_ = 0x00002000,     /* bit 13 */
+    // NOTIFICATION_TO_ = 0x00004000,     /* bit 14 */
+    // NOTIFICATION_TO_ = 0x00008000,     /* bit 15 */
+    // NOTIFICATION_TO_ = 0x00010000,     /* bit 16 */
+    // NOTIFICATION_TO_ = 0x00020000,     /* bit 17 */
+    // NOTIFICATION_TO_ = 0x00040000,     /* bit 18 */
+    // NOTIFICATION_TO_ = 0x00080000,     /* bit 19 */
+    // NOTIFICATION_TO_ = 0x00100000,     /* bit 20 */
+    // NOTIFICATION_TO_ = 0x00200000,     /* bit 21 */
+    // NOTIFICATION_TO_ = 0x00400000,     /* bit 22 */
+    // NOTIFICATION_TO_ = 0x00800000,     /* bit 23 */
+    // NOTIFICATION_TO_ = 0x01000000,     /* bit 24 */
+    // NOTIFICATION_TO_ = 0x02000000,     /* bit 25 */
+    // NOTIFICATION_TO_ = 0x04000000,     /* bit 26 */
+    // NOTIFICATION_TO_ = 0x08000000,     /* bit 27 */
+    // NOTIFICATION_TO_ = 0x10000000,     /* bit 28 */
+    // NOTIFICATION_TO_ = 0x20000000,     /* bit 29 */
+    // NOTIFICATION_TO_ = 0x40000000,     /* bit 30 */
+    // NOTIFICATION_TO_ = 0x80000000,     /* bit 31 */
+}
+notification_to_input_t
 
-/* Try to group according who is receiving thread, because only these message values cannot repeat. Perhaps use typedef enum for this. */
-#define NOTIFY_REQ_INIT_INPUT
-#define NOTIFY_REQ_INIT_OUTPUT
-#define NOTIFY_REQ_INIT_NETWORK
-#define NOTIFY_REQ_INIT_MEMORY
+typedef enum
+{
+    NOTIFICATION_TO_OUTPUT_REQ_INIT                     = 0x00000001,     /* bit 0 */
+    NOTIFICATION_TO_OUTPUT_REQ_HANDLE_EXT_LED           = 0x00000002,     /* bit 1 */
+    NOTIFICATION_TO_OUTPUT_REQ_HANDLE_PWR_HUMIDITY      = 0x00000004,     /* bit 2 */
+    NOTIFICATION_TO_OUTPUT_REQ_HANDLE_PWR_TEMPERATURE   = 0x00000008,     /* bit 3 */
+    NOTIFICATION_TO_OUTPUT_REQ_HANDLE_PWR_LIGHT         = 0x00000010,     /* bit 4 */
+//     NOTIFICATION_TO_ = 0x00000020,     /* bit 5 */
+//     NOTIFICATION_TO_ = 0x00000040,     /* bit 6 */
+//     NOTIFICATION_TO_ = 0x00000080,     /* bit 7 */
+//     NOTIFICATION_TO_ = 0x00000100,     /* bit 8 */
+//     NOTIFICATION_TO_ = 0x00000200,     /* bit 9 */
+//     NOTIFICATION_TO_ = 0x00000400,     /* bit 10 */
+//     NOTIFICATION_TO_ = 0x00000800,     /* bit 11 */
+//     NOTIFICATION_TO_ = 0x00001000,     /* bit 12 */
+//     NOTIFICATION_TO_ = 0x00002000,     /* bit 13 */
+//     NOTIFICATION_TO_ = 0x00004000,     /* bit 14 */
+//     NOTIFICATION_TO_ = 0x00008000,     /* bit 15 */
+//     NOTIFICATION_TO_ = 0x00010000,     /* bit 16 */
+//     NOTIFICATION_TO_ = 0x00020000,     /* bit 17 */
+//     NOTIFICATION_TO_ = 0x00040000,     /* bit 18 */
+//     NOTIFICATION_TO_ = 0x00080000,     /* bit 19 */
+//     NOTIFICATION_TO_ = 0x00100000,     /* bit 20 */
+//     NOTIFICATION_TO_ = 0x00200000,     /* bit 21 */
+//     NOTIFICATION_TO_ = 0x00400000,     /* bit 22 */
+//     NOTIFICATION_TO_ = 0x00800000,     /* bit 23 */
+//     NOTIFICATION_TO_ = 0x01000000,     /* bit 24 */
+//     NOTIFICATION_TO_ = 0x02000000,     /* bit 25 */
+//     NOTIFICATION_TO_ = 0x04000000,     /* bit 26 */
+//     NOTIFICATION_TO_ = 0x08000000,     /* bit 27 */
+//     NOTIFICATION_TO_ = 0x10000000,     /* bit 28 */
+//     NOTIFICATION_TO_ = 0x20000000,     /* bit 29 */
+//     NOTIFICATION_TO_ = 0x40000000,     /* bit 30 */
+//     NOTIFICATION_TO_ = 0x80000000,     /* bit 31 */
+}
+notification_to_output_t
 
-#define NOTIFY_RES_INIT_INPUT
-#define NOTIFY_RES_INIT_OUTPUT
-#define NOTIFY_RES_INIT_NETWORK
-#define NOTIFY_RES_INIT_MEMORY
+typedef enum
+{
+    NOTIFICATION_TO_APP_RES_INIT_INPUT                  = 0x00000001,     /* bit 0 */
+    NOTIFICATION_TO_APP_RES_INIT_OUTPUT                 = 0x00000002,     /* bit 1 */
+    NOTIFICATION_TO_APP_RES_INIT_NETWORK                = 0x00000004,     /* bit 2 */
+    NOTIFICATION_TO_APP_RES_INIT_MEMORY                 = 0x00000008,     /* bit 3 */
+    // NOTIFICATION_TO_ = 0x00000010,     /* bit 4 */
+    // NOTIFICATION_TO_ = 0x00000020,     /* bit 5 */
+    // NOTIFICATION_TO_ = 0x00000040,     /* bit 6 */
+    // NOTIFICATION_TO_ = 0x00000080,     /* bit 7 */
+    // NOTIFICATION_TO_ = 0x00000100,     /* bit 8 */
+    // NOTIFICATION_TO_ = 0x00000200,     /* bit 9 */
+    // NOTIFICATION_TO_ = 0x00000400,     /* bit 10 */
+    // NOTIFICATION_TO_ = 0x00000800,     /* bit 11 */
+    // NOTIFICATION_TO_ = 0x00001000,     /* bit 12 */
+    // NOTIFICATION_TO_ = 0x00002000,     /* bit 13 */
+    // NOTIFICATION_TO_ = 0x00004000,     /* bit 14 */
+    // NOTIFICATION_TO_ = 0x00008000,     /* bit 15 */
+    // NOTIFICATION_TO_ = 0x00010000,     /* bit 16 */
+    // NOTIFICATION_TO_ = 0x00020000,     /* bit 17 */
+    // NOTIFICATION_TO_ = 0x00040000,     /* bit 18 */
+    // NOTIFICATION_TO_ = 0x00080000,     /* bit 19 */
+    // NOTIFICATION_TO_ = 0x00100000,     /* bit 20 */
+    // NOTIFICATION_TO_ = 0x00200000,     /* bit 21 */
+    // NOTIFICATION_TO_ = 0x00400000,     /* bit 22 */
+    // NOTIFICATION_TO_ = 0x00800000,     /* bit 23 */
+    // NOTIFICATION_TO_ = 0x01000000,     /* bit 24 */
+    // NOTIFICATION_TO_ = 0x02000000,     /* bit 25 */
+    // NOTIFICATION_TO_ = 0x04000000,     /* bit 26 */
+    // NOTIFICATION_TO_ = 0x08000000,     /* bit 27 */
+    // NOTIFICATION_TO_ = 0x10000000,     /* bit 28 */
+    // NOTIFICATION_TO_ = 0x20000000,     /* bit 29 */
+    // NOTIFICATION_TO_ = 0x40000000,     /* bit 30 */
+    // NOTIFICATION_TO_ = 0x80000000,     /* bit 31 */
+}
+notification_to_app_t
 
-#define NOTIFY_REQ_HANDLE_INPUT
-/* result is arrived queue */
+typedef enum
+{
+    NOTIFICATION_TO_NETWORK_REQ_INIT                    = 0x00000001,     /* bit 0 */
+//     NOTIFICATION_TO_ = 0x00000002,     /* bit 1 */
+//     NOTIFICATION_TO_ = 0x00000004,     /* bit 2 */
+//     NOTIFICATION_TO_ = 0x00000008,     /* bit 3 */
+//     NOTIFICATION_TO_ = 0x00000010,     /* bit 4 */
+//     NOTIFICATION_TO_ = 0x00000020,     /* bit 5 */
+//     NOTIFICATION_TO_ = 0x00000040,     /* bit 6 */
+//     NOTIFICATION_TO_ = 0x00000080,     /* bit 7 */
+//     NOTIFICATION_TO_ = 0x00000100,     /* bit 8 */
+//     NOTIFICATION_TO_ = 0x00000200,     /* bit 9 */
+//     NOTIFICATION_TO_ = 0x00000400,     /* bit 10 */
+//     NOTIFICATION_TO_ = 0x00000800,     /* bit 11 */
+//     NOTIFICATION_TO_ = 0x00001000,     /* bit 12 */
+//     NOTIFICATION_TO_ = 0x00002000,     /* bit 13 */
+//     NOTIFICATION_TO_ = 0x00004000,     /* bit 14 */
+//     NOTIFICATION_TO_ = 0x00008000,     /* bit 15 */
+//     NOTIFICATION_TO_ = 0x00010000,     /* bit 16 */
+//     NOTIFICATION_TO_ = 0x00020000,     /* bit 17 */
+//     NOTIFICATION_TO_ = 0x00040000,     /* bit 18 */
+//     NOTIFICATION_TO_ = 0x00080000,     /* bit 19 */
+//     NOTIFICATION_TO_ = 0x00100000,     /* bit 20 */
+//     NOTIFICATION_TO_ = 0x00200000,     /* bit 21 */
+//     NOTIFICATION_TO_ = 0x00400000,     /* bit 22 */
+//     NOTIFICATION_TO_ = 0x00800000,     /* bit 23 */
+//     NOTIFICATION_TO_ = 0x01000000,     /* bit 24 */
+//     NOTIFICATION_TO_ = 0x02000000,     /* bit 25 */
+//     NOTIFICATION_TO_ = 0x04000000,     /* bit 26 */
+//     NOTIFICATION_TO_ = 0x08000000,     /* bit 27 */
+//     NOTIFICATION_TO_ = 0x10000000,     /* bit 28 */
+//     NOTIFICATION_TO_ = 0x20000000,     /* bit 29 */
+//     NOTIFICATION_TO_ = 0x40000000,     /* bit 30 */
+//     NOTIFICATION_TO_ = 0x80000000,     /* bit 31 */
+}
+notification_to_network_t
 
-#define NOTIFY_REQ_HANDLE_INPUT_BATTERY
-/* result is arrived queue */
+typedef enum
+{
+    NOTIFICATION_TO_MEMORY_REQ_INIT = 0x00000001,     /* bit 0 */
+//     NOTIFICATION_TO_ = 0x00000002,     /* bit 1 */
+//     NOTIFICATION_TO_ = 0x00000004,     /* bit 2 */
+//     NOTIFICATION_TO_ = 0x00000008,     /* bit 3 */
+//     NOTIFICATION_TO_ = 0x00000010,     /* bit 4 */
+//     NOTIFICATION_TO_ = 0x00000020,     /* bit 5 */
+//     NOTIFICATION_TO_ = 0x00000040,     /* bit 6 */
+//     NOTIFICATION_TO_ = 0x00000080,     /* bit 7 */
+//     NOTIFICATION_TO_ = 0x00000100,     /* bit 8 */
+//     NOTIFICATION_TO_ = 0x00000200,     /* bit 9 */
+//     NOTIFICATION_TO_ = 0x00000400,     /* bit 10 */
+//     NOTIFICATION_TO_ = 0x00000800,     /* bit 11 */
+//     NOTIFICATION_TO_ = 0x00001000,     /* bit 12 */
+//     NOTIFICATION_TO_ = 0x00002000,     /* bit 13 */
+//     NOTIFICATION_TO_ = 0x00004000,     /* bit 14 */
+//     NOTIFICATION_TO_ = 0x00008000,     /* bit 15 */
+//     NOTIFICATION_TO_ = 0x00010000,     /* bit 16 */
+//     NOTIFICATION_TO_ = 0x00020000,     /* bit 17 */
+//     NOTIFICATION_TO_ = 0x00040000,     /* bit 18 */
+//     NOTIFICATION_TO_ = 0x00080000,     /* bit 19 */
+//     NOTIFICATION_TO_ = 0x00100000,     /* bit 20 */
+//     NOTIFICATION_TO_ = 0x00200000,     /* bit 21 */
+//     NOTIFICATION_TO_ = 0x00400000,     /* bit 22 */
+//     NOTIFICATION_TO_ = 0x00800000,     /* bit 23 */
+//     NOTIFICATION_TO_ = 0x01000000,     /* bit 24 */
+//     NOTIFICATION_TO_ = 0x02000000,     /* bit 25 */
+//     NOTIFICATION_TO_ = 0x04000000,     /* bit 26 */
+//     NOTIFICATION_TO_ = 0x08000000,     /* bit 27 */
+//     NOTIFICATION_TO_ = 0x10000000,     /* bit 28 */
+//     NOTIFICATION_TO_ = 0x20000000,     /* bit 29 */
+//     NOTIFICATION_TO_ = 0x40000000,     /* bit 30 */
+//     NOTIFICATION_TO_ = 0x80000000,     /* bit 31 */
+}
+notification_to_memory_t
 
-#define NOTIFY_REQ_HANDLE_OUTPUT_PWR_HUMIDITY
-#define NOTIFY_REQ_HANDLE_OUTPUT_PWR_TEMPERATURE
-#define NOTIFY_REQ_HANDLE_OUTPUT_PWR_LIGHT
-
-#define NOTIFY_RES_HANDLE_OUTPUT_PWR_HUMIDITY
-#define NOTIFY_RES_HANDLE_OUTPUT_PWR_TEMPERATURE
-#define NOTIFY_RES_HANDLE_OUTPUT_PWR_LIGHT
+// typedef enum
+// {
+//     NOTIFICATION_TO_ = 0x00000001,     /* bit 0 */
+//     NOTIFICATION_TO_ = 0x00000002,     /* bit 1 */
+//     NOTIFICATION_TO_ = 0x00000004,     /* bit 2 */
+//     NOTIFICATION_TO_ = 0x00000008,     /* bit 3 */
+//     NOTIFICATION_TO_ = 0x00000010,     /* bit 4 */
+//     NOTIFICATION_TO_ = 0x00000020,     /* bit 5 */
+//     NOTIFICATION_TO_ = 0x00000040,     /* bit 6 */
+//     NOTIFICATION_TO_ = 0x00000080,     /* bit 7 */
+//     NOTIFICATION_TO_ = 0x00000100,     /* bit 8 */
+//     NOTIFICATION_TO_ = 0x00000200,     /* bit 9 */
+//     NOTIFICATION_TO_ = 0x00000400,     /* bit 10 */
+//     NOTIFICATION_TO_ = 0x00000800,     /* bit 11 */
+//     NOTIFICATION_TO_ = 0x00001000,     /* bit 12 */
+//     NOTIFICATION_TO_ = 0x00002000,     /* bit 13 */
+//     NOTIFICATION_TO_ = 0x00004000,     /* bit 14 */
+//     NOTIFICATION_TO_ = 0x00008000,     /* bit 15 */
+//     NOTIFICATION_TO_ = 0x00010000,     /* bit 16 */
+//     NOTIFICATION_TO_ = 0x00020000,     /* bit 17 */
+//     NOTIFICATION_TO_ = 0x00040000,     /* bit 18 */
+//     NOTIFICATION_TO_ = 0x00080000,     /* bit 19 */
+//     NOTIFICATION_TO_ = 0x00100000,     /* bit 20 */
+//     NOTIFICATION_TO_ = 0x00200000,     /* bit 21 */
+//     NOTIFICATION_TO_ = 0x00400000,     /* bit 22 */
+//     NOTIFICATION_TO_ = 0x00800000,     /* bit 23 */
+//     NOTIFICATION_TO_ = 0x01000000,     /* bit 24 */
+//     NOTIFICATION_TO_ = 0x02000000,     /* bit 25 */
+//     NOTIFICATION_TO_ = 0x04000000,     /* bit 26 */
+//     NOTIFICATION_TO_ = 0x08000000,     /* bit 27 */
+//     NOTIFICATION_TO_ = 0x10000000,     /* bit 28 */
+//     NOTIFICATION_TO_ = 0x20000000,     /* bit 29 */
+//     NOTIFICATION_TO_ = 0x40000000,     /* bit 30 */
+//     NOTIFICATION_TO_ = 0x80000000,     /* bit 31 */
+// }
+// notification_to__t
 
 /* ---------------------------- Public functions ---------------------------- */
 bool common_thread_objects_init(void);
