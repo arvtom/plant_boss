b71756cfc022f42c090e6a44292e0e382ffbfea2
arvtom <tomkusarvydas@gmail.com>
2023-07-30 17:19:32 +0300

    debug

--- sw_rpi/http_request_handler.py
+++ sw_rpi/http_request_handler.py
@@ -112,15 +112,12 @@ def handle_get_plot(start_line):
     for i in range(len(rows)-1, 0, -1):
         row = rows[i]
 
-        timestamp.append(row[1])
+        time.append(row[1])
         humidity.append(row[3])
         light.append(row[4])
         temperature.append(row[5])
 
-    time = range(0, len(rows)-1, 1)
-
-    print(timestamp[0])
-    print(str(timestamp[0]))
+    # time = range(0, len(rows)-1, 1)
 
     time = time[0:10]
     humidity = humidity[0:10]
