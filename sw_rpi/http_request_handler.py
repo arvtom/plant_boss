9ea6858446158160227fa6886c997321f226c558
arvtom <tomkusarvydas@gmail.com>
2023-08-03 18:17:55 +0300

    debug

--- sw_rpi/http_request_handler.py
+++ sw_rpi/http_request_handler.py
@@ -79,7 +79,7 @@ def handle_get_settings(start_line):
                 </tr>
                 """
 
-    for i in range(len(rows)-1, 0, -1):
+    for i in range(len(rows), 0, -1):
         row = rows[i]
         response_body += "<tr>"
         response_body += "<td>" + str(row[0]) + "</td>"         # entry_id
