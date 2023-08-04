3937d211eb23e1a76a18279919d32bfdc42e8645
arvtom <tomkusarvydas@gmail.com>
2023-08-04 18:29:55 +0300

    fix name

--- sw_rpi/test_rpi.sh
+++ sw_rpi/test_rpi.sh
@@ -1,7 +1,12 @@
 # The purpose of this script is to transfer plant_boss/sw_rpi folder from development machine to rpi and start plant_boss software automatically.
 
-scp -r /home/arvydas/github/plant_boss/sw_rpi pi@192.168.8.139:/home/pi/github/plant_boss/sw_rpi
+# Get pw_rpi
+source settings_test_rpi.sh
 
-ssh pi@192.168.8.139 "bash -s" /home/pi/github/plant_boss/sw_rpi/plant_boss.sh
+echo $pw_rpi
 
-echo "done"
+#scp -r /home/arvydas/github/plant_boss/sw_rpi pi@192.168.8.139:/home/pi/github/plant_boss/sw_rpi
+
+#ssh pi@192.168.8.139 "bash -s" /home/pi/github/plant_boss/sw_rpi/plant_boss.sh
+
+#echo "done"
