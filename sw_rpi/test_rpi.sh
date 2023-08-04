fca155bd38cb9d8297bd75443f9376aab7568ceb
arvtom <tomkusarvydas@gmail.com>
2023-08-04 18:37:38 +0300

    ignore logs

--- sw_rpi/test_rpi.sh
+++ sw_rpi/test_rpi.sh
@@ -1,12 +1,11 @@
 # The purpose of this script is to transfer plant_boss/sw_rpi folder from development machine to rpi and start plant_boss software automatically.
 
 # Get pw_rpi
-source settings_test_rpi.sh
+source settings_test_rpi.sh &> test_rpi.log
 
-echo $pw_rpi
-
-#scp -r /home/arvydas/github/plant_boss/sw_rpi pi@192.168.8.139:/home/pi/github/plant_boss/sw_rpi
+# Call scp to copy test scripts to rpi. Use sshpass to provide pass.
+sshpass -p $pw_rpi scp -r /home/arvydas/github/plant_boss/sw_rpi pi@192.168.8.139:/home/pi/github/plant_boss/sw_rpi &> test_rpi.log
 
 #ssh pi@192.168.8.139 "bash -s" /home/pi/github/plant_boss/sw_rpi/plant_boss.sh
 
-#echo "done"
+echo "test_rpi.sh done"
