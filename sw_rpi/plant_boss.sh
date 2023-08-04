563348e5310a18e4e279674b5f9f56988851bf53
arvtom <tomkusarvydas@gmail.com>
2023-08-04 18:24:27 +0300

    logging to file

--- sw_rpi/plant_boss.sh
+++ sw_rpi/plant_boss.sh
@@ -1,10 +1,13 @@
-sudo cp nginx/default /etc/nginx/sites-available/default
+# The purpose of this script is to start all necessary processes on rpi needed for plant_boss system, including: webpage, telegram bot.
+# Using &> to save standard output and standard error stream to log file.
 
-sudo systemctl stop nginx
-sudo systemctl start nginx
+sudo cp nginx/default /etc/nginx/sites-available/default &> plant_boss.log
+
+sudo systemctl stop nginx &> plant_boss.log
+sudo systemctl start nginx &> plant_boss.log
 
 #Init server which writes/reads from database
-uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py
+uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py &> plant_boss.log
 # nohup uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py > uwsgi_plant_boss.log
 
 #Check if server accepts data from other devices
