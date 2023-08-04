e08fd70571ffb9c3fd03b66f710908caf65a8927
arvtom <tomkusarvydas@gmail.com>
2023-08-04 18:17:32 +0300

    Script to automate testing on rpi

---
+++ sw_rpi/test_rpi.sh
@@ -0,0 +1,7 @@
+# The purpose of this script is to transfer plant_boss/sw_rpi folder from development machine to rpi and start plant_boss software automatically.
+
+scp -r /home/arvydas/github/plant_boss/sw_rpi pi@192.168.8.139:/home/pi/github/plant_boss/sw_rpi
+
+ssh pi@192.168.8.139 "bash -s" /home/pi/github/plant_boss/sw_rpi/plant_boss.sh
+
+echo "done"
