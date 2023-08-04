c615a5325f3ea485fb8cc85b6427ab7dc6936a11
arvtom <tomkusarvydas@gmail.com>
2023-08-04 18:52:20 +0300

    ignore all logs. Test automation

--- sw_rpi/plant_boss.sh
+++ sw_rpi/plant_boss.sh
@@ -1,19 +1,11 @@
 # The purpose of this script is to start all necessary processes on rpi needed for plant_boss system, including: webpage, telegram bot.
 # Using &> to save standard output and standard error stream to log file.
 
-sudo cp nginx/default /etc/nginx/sites-available/default &> plant_boss.log
+# sudo cp nginx/default /etc/nginx/sites-available/default &> plant_boss.log
 
-sudo systemctl stop nginx &> plant_boss.log
-sudo systemctl start nginx &> plant_boss.log
+# sudo systemctl stop nginx &> plant_boss.log
+# sudo systemctl start nginx &> plant_boss.log
 
-#Init server which writes/reads from database
-uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py &> plant_boss.log
-# nohup uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py > uwsgi_plant_boss.log
-
-#Check if server accepts data from other devices
-
-#curl -X POST -d "timestamp=1.39&device=1&humidity=1.49&light=1.59&temperature=1.69&bat_voltage=1.79&rssi_wifi=1.89" 192.168.8.139:9999/
-
-#Check if data was put into database from other devices
-
-#curl 192.168.8.139:9999/
+# #Init server which writes/reads from database
+# uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py &> plant_boss.log
+echo "plant_boss.sh done" &> plant_boss.log
--- sw_rpi/test_rpi.sh
+++ sw_rpi/test_rpi.sh
@@ -1,11 +1,11 @@
 # The purpose of this script is to transfer plant_boss/sw_rpi folder from development machine to rpi and start plant_boss software automatically.
 
-# Get pw_rpi
+# Get pw_rpi, path_scp and path_ssh
 source settings_test_rpi.sh &> test_rpi.log
 
 # Call scp to copy test scripts to rpi. Use sshpass to provide pass.
-sshpass -p $pw_rpi scp -r /home/arvydas/github/plant_boss/sw_rpi pi@192.168.8.139:/home/pi/github/plant_boss/sw_rpi &> test_rpi.log
+sshpass -p $pw_rpi scp -r /home/arvydas/github/plant_boss/sw_rpi $path_scp &> test_rpi.log
 
-#ssh pi@192.168.8.139 "bash -s" /home/pi/github/plant_boss/sw_rpi/plant_boss.sh
+sshpass -p $pw_rpi ssh path_ssh "bash -s" /home/pi/github/plant_boss/sw_rpi/plant_boss.sh &> test_rpi.log
 
 echo "test_rpi.sh done"
