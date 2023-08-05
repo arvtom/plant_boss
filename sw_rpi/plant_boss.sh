383c2acfd4662f7e32021e3ded1c8ed7b653c789
arvtom <tomkusarvydas@gmail.com>
2023-08-05 13:34:58 +0300

    nohup works to run process in background and return to shell. However killall does not work

--- sw_rpi/plant_boss.sh
+++ sw_rpi/plant_boss.sh
@@ -6,7 +6,10 @@ sudo cp nginx/default /etc/nginx/sites-available/default
 sudo systemctl stop nginx
 sudo systemctl start nginx
 
-#Init server which writes/reads from database
-# uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py &> uwsgi.log
+# Stop existing uwsgi process
+killall uwsgi
+
+# Start uwsgi as a background process
+nohup bash -c "uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py" &> uwsgi.log &
 
 echo "plant_boss.sh done"
