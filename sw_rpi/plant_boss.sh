d14622a6c008ec6befd69f7a9f3bfeae7f8f4cb6
arvtom <tomkusarvydas@gmail.com>
2023-08-05 14:20:56 +0300

    pause after killall does not seem to help. Need to check using while if port is free.

--- sw_rpi/plant_boss.sh
+++ sw_rpi/plant_boss.sh
@@ -7,7 +7,10 @@ sudo systemctl stop nginx
 sudo systemctl start nginx
 
 # Stop existing uwsgi process
-killall uwsgi
+killall -v uwsgi
+
+# Wait 5s until uwsgi releases 8001 port
+read -t 10
 
 # Start uwsgi as a background process
 nohup bash -c "uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py" &> uwsgi.log &
