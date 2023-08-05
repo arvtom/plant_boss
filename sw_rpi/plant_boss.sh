07a4d6d4cd0402f1424d3cf08fcf515d5286aca8
arvtom <tomkusarvydas@gmail.com>
2023-08-05 12:35:57 +0300

    now calling script via ssh is working

--- sw_rpi/plant_boss.sh
+++ sw_rpi/plant_boss.sh
@@ -1,11 +1,12 @@
 # The purpose of this script is to start all necessary processes on rpi needed for plant_boss system, including: webpage, telegram bot.
 # Using &> to save standard output and standard error stream to log file.
 
-# sudo cp nginx/default /etc/nginx/sites-available/default &> plant_boss.log
+sudo cp nginx/default /etc/nginx/sites-available/default
 
-# sudo systemctl stop nginx &> plant_boss.log
-# sudo systemctl start nginx &> plant_boss.log
+sudo systemctl stop nginx
+sudo systemctl start nginx
 
-# #Init server which writes/reads from database
-# uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py &> plant_boss.log
-echo "plant_boss.sh done" &> plant_boss.log
+#Init server which writes/reads from database
+# uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py &> uwsgi.log
+
+echo "plant_boss.sh done"
--- sw_rpi/test_rpi.sh
+++ sw_rpi/test_rpi.sh
@@ -1,12 +1,12 @@
 # The purpose of this script is to transfer plant_boss/sw_rpi folder from development machine to rpi and start plant_boss software automatically.
 
 # Get pw_rpi, path_scp and path_ssh
-source settings_test_rpi.sh &> test_rpi.log
+source settings_test_rpi.sh
 
 # Call scp to copy test scripts to rpi. Use sshpass to provide pass.
-sshpass -p $pw_rpi scp -r /home/arvydas/github/plant_boss/sw_rpi $path_scp &> test_rpi.log
+sshpass -p $pw_rpi scp -r /home/arvydas/github/plant_boss/sw_rpi $path_scp
 
-# sshpass -p $pw_rpi ssh $path_ssh "bash -s" < pi@192.168.8.139:/home/arvydas/github/plant_boss/sw_rpi/plant_boss.sh &> test_rpi.log
-echo "bash /home/pi/github/plant_boss/sw_rpi/plant_boss.sh" | sshpass -p $pw_rpi ssh pi@192.168.8.139
+# Call plant_boss script to start all needed processes on rpi
+echo "cd /home/pi/github/plant_boss/sw_rpi; bash plant_boss.sh &> plant_boss.log" | sshpass -p $pw_rpi ssh pi@192.168.8.139
 
 echo "test_rpi.sh done"
