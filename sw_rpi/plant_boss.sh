192b9f9e21fee606646f985e3bbb0afd9a183a65
arvtom <tomkusarvydas@gmail.com>
2023-08-05 15:27:24 +0300

    sucessful kill and restart of uwsgi using script

--- sw_rpi/plant_boss.sh
+++ sw_rpi/plant_boss.sh
@@ -9,8 +9,8 @@ sudo systemctl start nginx
 # Stop existing uwsgi process
 killall -v uwsgi
 
-# Wait 5s until uwsgi releases 8001 port
-read -t 10
+# Wait 2s until previous uwsgi process releases port
+sleep 2
 
 # Start uwsgi as a background process
 nohup bash -c "uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py" &> uwsgi.log &
