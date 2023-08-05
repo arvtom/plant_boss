7f1841fb1accaa653a83c921451bec65a929a111
arvtom <tomkusarvydas@gmail.com>
2023-08-05 15:40:16 +0300

    Working automatic starting of uwsgi and telegram bot

--- sw_rpi/plant_boss.sh
+++ sw_rpi/plant_boss.sh
@@ -9,10 +9,16 @@ sudo systemctl start nginx
 # Stop existing uwsgi process
 killall -v uwsgi
 
-# Wait 2s until previous uwsgi process releases port
-sleep 2
+# Stop existing process of telegram_bot.py
+killall -v python
+
+# Wait 5s until previous uwsgi process releases port and python closes.
+sleep 5
 
 # Start uwsgi as a background process
-nohup bash -c "uwsgi --socket 127.0.0.1:8001 --wsgi-file /home/pi/github/plant_boss/sw_rpi/http_request_handler.py" &> uwsgi.log &
+nohup bash -c "uwsgi --socket 127.0.0.1:8001 --wsgi-file http_request_handler.py" &> uwsgi.log &
+
+# Start telegram_bot.py as a background process
+nohup bash -c "python telegram_bot.py" &> telegram_bot.log &
 
 echo "plant_boss.sh done"
