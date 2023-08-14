5df49d3d4106bc0c5b668dc2e61188db19c90d18
arvtom <tomkusarvydas@gmail.com>
2023-08-14 20:40:08 +0300

    With such config telegram_bot keeps running, however uwsgi is killed. It is necessary to have speerate systemd processes for uwsgi and telegram_bot, to keep them running in parallel

--- sw_rpi/plant_boss.sh
+++ sw_rpi/plant_boss.sh
@@ -1,24 +1,28 @@
 # The purpose of this script is to start all necessary processes on rpi needed for plant_boss system, including: webpage, telegram bot.
 # Using &> to save standard output and standard error stream to log file. Using & to return executing next bash commands.
 
-sudo cp nginx/default /etc/nginx/sites-available/default
+cd /home/pi/github/plant_boss/sw_rpi
 
-sudo systemctl stop nginx
-sudo systemctl start nginx
+ls &> log/plant_boss.log
+
+sudo cp nginx/default /etc/nginx/sites-available/default &> log/plant_boss.log
+
+sudo systemctl stop nginx &> log/plant_boss.log
+sudo systemctl start nginx &> log/plant_boss.log
 
 # Stop existing uwsgi process
 killall -v uwsgi
 
 # Stop existing process of telegram_bot.py
-killall -v python
+killall -v python &> log/plant_boss.log
 
 # Wait 5s until previous uwsgi process releases port and python closes.
-sleep 5
+sleep 5 &> log/plant_boss.log
 
 # Start uwsgi as a background process
-uwsgi --socket 127.0.0.1:8001 --wsgi-file http_request_handler.py &> log/uwsgi.log &
+/home/pi/.local/bin/uwsgi --socket 127.0.0.1:8001 --wsgi-file http_request_handler.py &> log/uwsgi.log &
 
 # Start telegram_bot.py as a background process
 python telegram_bot.py &> log/telegram_bot.log &
 
-echo "plant_boss.sh done"
+echo "plant_boss.sh done" &> log/plant_boss.log
