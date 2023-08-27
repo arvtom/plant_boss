4ff51bbaf34bc34174f98acafe98c7ebd5dec490
arvtom <tomkusarvydas@gmail.com>
2023-08-27 16:28:50 +0300

    some example nvs code is working

--- sw_esp32/plant_boss/src/thread_memory.c
+++ sw_esp32/plant_boss/src/thread_memory.c
@@ -106,55 +106,54 @@ void nvs_handle_savings(void)
     // ESP_ERROR_CHECK( err_nvs );
 
     // Open
-    printf("1");
-    // printf("Opening Non-Volatile Storage (NVS) handle... ");
-    // nvs_handle_t my_handle;
-    // err_nvs = nvs_open("storage", NVS_READWRITE, &my_handle);
-    // if (err_nvs != ESP_OK) {
-    //     printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err_nvs));
-    // } 
-    // else 
-    // {
-    //     printf("Done\n");
+    printf("Opening Non-Volatile Storage (NVS) handle... ");
+    nvs_handle_t my_handle;
+    esp_err_t err_nvs = nvs_open("storage", NVS_READWRITE, &my_handle);
+    if (err_nvs != ESP_OK) {
+        printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err_nvs));
+    } 
+    else 
+    {
+        printf("Done\n");
 
-    //     // Read
-    //     printf("Reading restart counter from NVS ... ");
-    //     int32_t restart_counter = 0; // value will default to 0, if not set yet in NVS
-    //     err_nvs = nvs_get_i32(my_handle, "restart_counter", &restart_counter);
-    //     switch (err_nvs) 
-    //     {
-    //         case ESP_OK:
-    //             printf("Done\n");
-    //             printf("Restart counter = %ld\n", restart_counter);
-    //             break;
-    //         case ESP_ERR_NVS_NOT_FOUND:
-    //             printf("The value is not initialized yet!\n");
-    //             break;
-    //         default :
-    //             printf("Error (%s) reading!\n", esp_err_to_name(err_nvs));
-    //     }
+        // Read
+        printf("Reading restart counter from NVS ... ");
+        int32_t restart_counter = 0; // value will default to 0, if not set yet in NVS
+        err_nvs = nvs_get_i32(my_handle, "restart_counter", &restart_counter);
+        switch (err_nvs) 
+        {
+            case ESP_OK:
+                printf("Done\n");
+                printf("Restart counter = %ld\n", restart_counter);
+                break;
+            case ESP_ERR_NVS_NOT_FOUND:
+                printf("The value is not initialized yet!\n");
+                break;
+            default :
+                printf("Error (%s) reading!\n", esp_err_to_name(err_nvs));
+        }
 
-    //     //TODO: read filter structure
+        //TODO: read filter structure
 
-    //     // Write
-    //     printf("Updating restart counter in NVS ... ");
-    //     restart_counter++;
-    //     err_nvs = nvs_set_i32(my_handle, "restart_counter", restart_counter);
-    //     printf((err_nvs != ESP_OK) ? "Failed!\n" : "Done\n");
+        // Write
+        printf("Updating restart counter in NVS ... ");
+        restart_counter++;
+        err_nvs = nvs_set_i32(my_handle, "restart_counter", restart_counter);
+        printf((err_nvs != ESP_OK) ? "Failed!\n" : "Done\n");
 
-    //     //TODO: save filter structure
+        //TODO: save filter structure
 
-    //     // Commit written value.
-    //     // After setting any values, nvs_commit() must be called to ensure changes are written
-    //     // to flash storage. Implementations may write to storage at other times,
-    //     // but this is not guaranteed.
-    //     printf("Committing updates in NVS ... ");
-    //     err_nvs = nvs_commit(my_handle);
-    //     printf((err_nvs != ESP_OK) ? "Failed!\n" : "Done\n");
+        // Commit written value.
+        // After setting any values, nvs_commit() must be called to ensure changes are written
+        // to flash storage. Implementations may write to storage at other times,
+        // but this is not guaranteed.
+        printf("Committing updates in NVS ... ");
+        err_nvs = nvs_commit(my_handle);
+        printf((err_nvs != ESP_OK) ? "Failed!\n" : "Done\n");
 
-    //     // Close
-    //     nvs_close(my_handle);
-    // }
+        // Close
+        nvs_close(my_handle);
+    }
 }
 
 /* ---------------------------- Interrupt callbacks ---------------------------- */
