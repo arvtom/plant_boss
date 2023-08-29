4029aa6f6a484d26812be8e79b4171c3b38e5802
arvtom <tomkusarvydas@gmail.com>
2023-08-29 16:23:00 +0300

    now scp seems fixed

--- sw_rpi/test_rpi.sh
+++ sw_rpi/test_rpi.sh
@@ -5,8 +5,10 @@ source settings_test_rpi.sh
 
 # Call scp to copy test scripts to rpi. Use sshpass to provide pass.
 # Copy all contents of sw_rpi except .db file.
+cd /home/arvydas/github/plant_boss/sw_rpi
 shopt -s extglob
-sshpass -p $pw_rpi scp -r /home/arvydas/github/plant_boss/sw_rpi/!(plant_boss.db) $path_scp
+# sshpass -p $pw_rpi scp /home/arvydas/github/plant_boss/sw_rpi/!(plant_boss.db) -r $path_scp
+sshpass -p $pw_rpi scp -r !(plant_boss.db) $path_scp
 
 # Call plant_boss script to start all needed processes on rpi
 echo "bash /home/pi/github/plant_boss/sw_rpi/plant_boss.sh" | sshpass -p $pw_rpi ssh pi@192.168.1.101
