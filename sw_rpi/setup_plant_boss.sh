d56b43ffb19a2feac9beca5d2cdf2172e9a0cfeb
arvtom <tomkusarvydas@gmail.com>
2023-09-01 00:49:15 +0300

    notes for rpi setup

--- sw_rpi/setup_plant_boss.sh
+++ sw_rpi/setup_plant_boss.sh
@@ -1,45 +1,54 @@
 #According to: https://opensource.com/article/21/3/web-hosting-raspberry-pi
 
-#Install packages
+# Linux: 2023-05-03-raspios-bullseye-armhf.img.xz.
 
-#sudo apt-get install sqlite3 build-essential python3 nginx matplotlib
-#pip install uwsgi
+#Install packages
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install sqlite3 nginx
+#pip install uwsgi telebot APScheduler
+
+# This seems quicker to install matplotlib, since pip tries to build matplotlib from source on rpi zero, which is painfully slow.
+#sudo apt install python3-matplotlib
 
 #Create and open database
 
 #sqlite3 /home/pi/plant_boss/plant_boss.db
 
-#Create new table in database
+#Create new data table in sqlite3
+CREATE TABLE plant_boss_test_3 (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+timestamp TEXT NOT NULL, 
+device INTEGER NOT NULL, 
+humidity DECIMAL NOT NULL, 
+light DECIMAL NOT NULL, 
+temperature DECIMAL NOT NULL,
+bat_voltage DECIMAL NOT NULL,
+rssi_wifi INTEGER NOT NULL,
+mode INTEGER NOT NULL,
+bat_low_flag INTEGER NOT NULL,
+error_app INTEGER NOT NULL,
+error_input INTEGER NOT NULL,
+error_output INTEGER NOT NULL,
+error_network INTEGER NOT NULL,
+error_memory INTEGER NOT NULL,
+sw_version INTEGER NOT NULL,
+timer_or_period INTEGER NOT NULL);
 
-# CREATE TABLE plant_boss_test_3 (
-# id INTEGER PRIMARY KEY AUTOINCREMENT,
-# timestamp TEXT NOT NULL, 
-# device INTEGER NOT NULL, 
-# humidity DECIMAL NOT NULL, 
-# light DECIMAL NOT NULL, 
-# temperature DECIMAL NOT NULL,
-# bat_voltage DECIMAL NOT NULL,
-# rssi_wifi INTEGER NOT NULL,
-# mode INTEGER NOT NULL,
-# bat_low_flag INTEGER NOT NULL,
-# error_app INTEGER NOT NULL,
-# error_input INTEGER NOT NULL,
-# error_output INTEGER NOT NULL,
-# error_network INTEGER NOT NULL,
-# error_memory INTEGER NOT NULL,
-# sw_version INTEGER NOT NULL,
-# timer_or_period INTEGER NOT NULL);
-
-# CREATE TABLE plant_boss_settings_test_4 (
-# id INTEGER PRIMARY KEY AUTOINCREMENT,
-# timestamp TEXT NOT NULL, 
-# device INTEGER NOT NULL, 
-# mode INTEGER NOT NULL,
-# threshold_bat_voltage DECIMAL NOT NULL,
-# period_measurement DECIMAL NOT NULL);
+#Create new settings table in sqlite3
+CREATE TABLE plant_boss_settings_test_4 (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+timestamp TEXT NOT NULL, 
+device INTEGER NOT NULL, 
+mode INTEGER NOT NULL,
+threshold_bat_voltage DECIMAL NOT NULL,
+period_measurement DECIMAL NOT NULL);
 
 # To delete all rows in table:
-# DELETE FROM plant_boss_settings_test_3;
+DELETE FROM plant_boss_settings_test_3;
+
+# To delete table:
+DROP TABLE plant_boss_test_3
 
 #Show database structure
 
