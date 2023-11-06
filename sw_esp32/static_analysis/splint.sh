5800c1136a5a59a8a30172a75265db12dd32705f
arvtom <tomkusarvydas@gmail.com>
2023-11-06 21:17:11 +0200

    try to disable preprocessor errors

--- sw_esp32/static_analysis/splint.sh
+++ sw_esp32/static_analysis/splint.sh
@@ -1,13 +1,12 @@
 splint \
 ../plant_boss/src/* \
-../plant_boss/drivers/* \
-../plant_boss/wrappers/* \
-+skip-sys-headers \
--sysdirs ~/esp/esp-idf/components \
--unrecog +show-summary +stats -preproc \
--I ../plant_boss/inc \
--I ../plant_boss/drivers \
--I ../plant_boss/wrappers \
+-I../plant_boss/inc \
+-preproc \
 &> splint.txt
 
 cat splint.txt
+
+# ../plant_boss/drivers/* \
+# ../plant_boss/wrappers/* \
+# -I../plant_boss/drivers \
+# -I../plant_boss/wrappers \
