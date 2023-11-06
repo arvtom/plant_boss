6987b624b4653d56c3f9b7fdb36910e4221806d9
arvtom <tomkusarvydas@gmail.com>
2023-11-06 21:36:46 +0200

    Attempt to declare types which are not the scope of analysis as int.

--- sw_esp32/static_analysis/splint.sh
+++ sw_esp32/static_analysis/splint.sh
@@ -1,7 +1,17 @@
 splint \
+-preproc \
+\
+-Dadc_cali_handle_t=int \
+-Dadc_oneshot_chan_cfg_t=int \
+-Dadc_cali_line_fitting_config_t=int \
+-DSemaphoreHandle_t=int \
+-DQueueHandle_t=int \
+-DTaskHandle_t=int \
+-DUBaseType_t=int \
+-Dnvm_contents_t=int \
+\
 ../plant_boss/src/* \
 -I../plant_boss/inc \
--preproc \
 &> splint.txt
 
 cat splint.txt
@@ -10,3 +20,5 @@ cat splint.txt
 # ../plant_boss/wrappers/* \
 # -I../plant_boss/drivers \
 # -I../plant_boss/wrappers \
+
+# -D=int \
