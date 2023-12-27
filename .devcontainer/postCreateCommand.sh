ad50a3aefd578c8106da789cd550b2420fb0f865
arvtom <tomkusarvydas@gmail.com>
2023-12-27 19:45:04 +0000

    attempt to build container. However does not compile esp32

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt-get -y install cppcheck sshpass python3-pip python3-venv libusb-1.0-0-dev
+pip install flawfinder lizard
+
+# esp-idf also asks for:
+# sudo cp -n /root/.espressif/tools/openocd-esp32/v0.12.0-esp32-20230921/openocd-esp32/share/openocd/contrib/60-openocd.rules /etc/udev/rules.d
