98e8b46ace2a9e99875dbfa2e7106c2dbdba8d8b
Arvydas Tomkus <145611063+agnebiel@users.noreply.github.com>
2023-12-30 16:43:12 +0000

    Some docker image init changes

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,16 @@
 apt-get update
-apt-get -y install cppcheck sshpass python3-pip python3-venv libusb-1.0-0-dev minicom
+apt-get -y install sshpass python3-pip python3-venv libusb-1.0-0-dev minicom wget cmake
 pip install flawfinder lizard
 
+# Get newest version of cppcheck
+cd /home
+wget https://github.com/danmar/cppcheck/archive/refs/tags/2.13.0.tar.gz
+tar -xf 2.13.0.tar.gz
+cd cppcheck-2.13.0
+mkdir build &&
+cd build &&
+cmake .. &&
+cmake --build .
+
 # esp-idf also asks for:
-# sudo cp -n /root/.espressif/tools/openocd-esp32/v0.12.0-esp32-20230921/openocd-esp32/share/openocd/contrib/60-openocd.rules /etc/udev/rules.d
+# sudo cp -n /root/.espressif/tools/openocd-esp32/v0.12.0-esp32-20230921/openocd-esp32/share/openocd/contrib/60-openocd.rules /etc/udev/rules.dcd ..
