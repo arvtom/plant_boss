cc4cf24fa48a136f404aa64b6f29579106771276
Arvydas Tomkus <tomkusarvydas@gmail.com>
2024-02-24 13:49:28 +0200

    Print image size, first and last bytes.

--- sw_esp32/mem_crc/flash_image_crc.py
+++ sw_esp32/mem_crc/flash_image_crc.py
@@ -1,1 +1,21 @@
 # The purpose of this python script is to calculate esp32 flash image CRC and embed it's value in image.
+
+PATH_TO_IMAGE="../plant_boss/build/plant_boss.bin"
+
+print("python start")
+
+# with open(PATH_TO_IMAGE, 'r') as fp:
+#     hex_list = ["{:02x}".format(ord(c)) for c in fp.read()]
+
+# print("hexlist=" + hex_list[:10])
+
+file = open(PATH_TO_IMAGE, 'rb')
+
+image = file.read()
+image_size = len(image)
+
+print("size of image: " + str(image_size))
+print("first 10 bytes of image: " + str(image[:10]))
+print("last 10 bytes of image: " + str(image[image_size-10:]))
+
+print("python finish")
