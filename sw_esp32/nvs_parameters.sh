0f9934f4570cc1fbc4befb3ba78530f5059af008
Arvydas Tomkus <tomkusarvydas@gmail.com>
2024-03-23 22:09:30 +0200

    attempt to call embedded python in esp-idf

---
+++ sw_esp32/nvs_parameters.sh
@@ -0,0 +1,5 @@
+ls
+echo "hi"
+pwd
+/mnt/c/Users/Arvydas/.espressif/tools/idf-python/3.11.2/python.exe C:/Users/Arvydas/esp/v5.1.2/esp-idf/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py generate C:/repo/plant_boss/sw_esp32/nvs_parameters.csv C:/repo/plant_boss/sw_esp32/nvs_parameters.bin 0x6000
+# C:\Users\Arvydas\.espressif\tools\idf-python\3.11.2\python.exe C:/Users/Arvydas/esp/v5.1.2/esp-idf/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py generate C:\repo\plant_boss\sw_esp32\nvs_parameters.csv C:\repo\plant_boss\sw_esp32\nvs_parameters.bin 0x6000
